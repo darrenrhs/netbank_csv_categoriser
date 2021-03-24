093e32a6d3c031413059d69701dbd42a36f24720
darrenrhs <darrenrhs@gmail.com>
2021-03-24 14:44:55 +1100

    test

--- categories.py
+++
@@ -1,6 +0,0 @@
-# Some examples provided here, you can add your own based on how transactions are listed in your CSV. Names are case-sensitive.
-
-categories = {
-        "groceries": ["ALDI STORES", "WOOLWORTHS", "COLES"], 
-        "transaction fees": ["International Transaction Fee"]
-        } gyugyug
